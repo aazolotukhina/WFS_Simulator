060712cedb03f31ddea3460d277387c58517dcb8
aazolotukhina <anastasiazolotuhina17@gmail.com>
2022-02-14 00:15:53 +0300

    Create general case branch with matrices

--- Matrics_Simulator_general_case.m
+++ Matrics_Simulator_general_case.m
@@ -1,16 +1,14 @@
-clear;  
-clc;
-close all;
+close all; clear; clc;
 % Generates a hartmannograms for further processing in the WFS7
 
 %%  SHWFS parameters
 Lambda=0.65e-6;                 % wavelength [m]
 k=2*pi/Lambda;                  % wavenumber
 
-Pixels_X=500;                   % image pixels on the X axis
-Pixels_Y=500;                   % image pixels on the Y axis
-PixelSize=5.5e-6;               % pixel size [m]
-Pitch=137.5e-6;                   % microlens size [m]
+Pixels_X=2048;                   % image pixels on the X axis
+Pixels_Y=2048;                   % image pixels on the Y axis
+PixelSize= 5.5e-6;               % pixel size [m]
+Pitch=136e-6;                 % microlens size [m]
 ML_focal=3.2e-3;                % focal dist [m]
 Bits=8;                         % camera bit rate
 ApertDiam=Pixels_X*PixelSize;   % diameter of the full aperture [m]
@@ -18,23 +16,23 @@ ApertRad=ApertDiam/2;           % radius of the full aperture [m]
 
 L_X=Pixels_X*PixelSize;                    % sensor area on the X axis [m]
 L_Y=Pixels_Y*PixelSize;                    % sensor area on the Y axis [m]
-ML_Pixels=Pitch/PixelSize;                 % pixels in CCD to each microlens
+ML_Pixels=round(Pitch/PixelSize);          % pixels in CCD to each microlens
 
-NumberLens_X=floor(Pixels_X/ML_Pixels);    % number of lenses in the array 
-NumberLens_Y=floor(Pixels_Y/ML_Pixels);    % on the X and Y axis
+NumberLens_X=ceil(Pixels_X/ML_Pixels);    % number of lenses in the array 
+NumberLens_Y=ceil(Pixels_Y/ML_Pixels);    % on the X and Y axis
 
 %%  Setting the zernike coefficients
 % Set through the distance from the source to the sensor:
-% L_def=0.2;                                         %distance between the source and the SHWFS [m]
-% Defocus=(L_def-sqrt(L_def^2-(ApertDiam/2)^2))/2;    %in [m]  
+L_def=1.65;                                         %distance between the source and the SHWFS [m]
+Defocus=(L_def-sqrt(L_def^2-(ApertDiam/2)^2))/2;    %in [m]  
 
 % Set the parameters individually:
-Y_Tilt = 0;
+X_Tilt = 0;
 Defocus = 1e-6;
 
 % Aberration coefficients
-z11=Y_Tilt;         % Y-Tilt
-zi11=0;             % X-Tilt
+z11=X_Tilt;         % X-Tilt
+zi11=0;             % Y-Tilt
 z02=Defocus;        % Defocus 
 z22=0;              % Vertical astigmatism
 zi22=0;             % Oblique astigmatism
@@ -43,36 +41,51 @@ zi13=0;             % Vertical coma
 z04=0;              % Primary spherical
 
 %%  Reference
-%IntXY_Ref=zeros(ML_Pixels);
-H_ref={};
-ab = 1:ML_Pixels; cd = ab;
-% Diffraction spot in one subaperture
-x1 = -Pitch/2 + PixelSize*ab - PixelSize/2;
-y1 = -Pitch/2 + PixelSize*cd - PixelSize/2;
-[X1,Y1] = meshgrid(x1,y1);
-Intensity1 =(sinc(Pitch*X1/Lambda/ML_focal)).^2.*(sinc(Pitch*Y1/Lambda/ML_focal)).^2;
-% Сonversion to digitized intensity in the camera (by bit)
-MaxIntens=max(max(Intensity1(:,:),[],1));
-MinIntens=MaxIntens/(2^Bits-1);
-IntensityPixelised=fix(Intensity1(:,:)./MinIntens).*MinIntens/max(max(Intensity1)); 
-% Сombining into a single matrix (reference hartmannogram)
-for N=1:NumberLens_X
-    for M=1:NumberLens_Y
-        H_ref{N,M}=IntensityPixelised;
-    end
-end
-H_ref=cell2mat(H_ref);
-% Output of the reference hartmannogram
-figure('Name','Reference hartmannogram');
-imshow(H_ref,[]);
-% Save to the "Hartmannograms" folder 
-imwrite(H_ref,'Hartmannograms\without_integr_m.bmp'); 
+iRows = ceil(Pixels_X/(2*ML_Pixels)); 
+iCols = iRows;
+iRefRows = 2*iRows + 1;
+iRefCols = iRefRows;
 
+for n = 0 : 2*iRows
+    dx(n+1) = Pitch/PixelSize*(iRows - n);
+end
+x_0 = Pixels_X/2 - dx;
+y_0 = x_0;
+x_0_rep = repelem(x_0,ML_Pixels);
+y_0_rep = repelem(y_0,ML_Pixels);
+[X_0,Y_0] = meshgrid(x_0_rep,y_0_rep);
+p0_x = round(y_0 - ML_Pixels/2);
+p0_y = round(x_0 - ML_Pixels/2);
+P0_x = repelem(p0_x,ML_Pixels);
+P0_y = repelem(p0_y,ML_Pixels);
+[P0_X,P0_Y] = meshgrid(P0_x,P0_y);
+i = 1:ML_Pixels;
+k = 1:ML_Pixels;
+[I,K] = meshgrid(i,k);
+i_rep = repmat(I,NumberLens_X+1,NumberLens_X+1);
+k_rep = repmat(K,NumberLens_X+1,NumberLens_X+1);
+
+IX =  P0_X + k_rep - 1;
+IY =  P0_Y + i_rep - 1;
+
+X = IX - X_0;
+Y = IY - Y_0;
+
+VAL = ((sinc(Pitch*PixelSize*X/Lambda/ML_focal)).^2.*(sinc(Pitch*PixelSize*Y/Lambda/ML_focal)).^2);
+MaxIntens=max(max(VAL(:,:),[],1));
+MinIntens=MaxIntens/(2^Bits-1);
+IntensityPixelised_VAL=fix(VAL(:,:)./MinIntens).*MinIntens/max(max(VAL));
+
+figure('Name','Reference hartmannogram');
+imshow(IntensityPixelised_VAL,[]);
+% Save to the "Hartmannograms" folder 
+imwrite(IntensityPixelised_VAL,'Hartmannograms\REF_2048_136_val.bmp'); 
+                
 %%  Introduction slopes
 % Сoordinates of subaperture centers 
 ML_Center = Pitch/2;
 k=1;
-for i=0:NumberLens_X-1
+for i=0:NumberLens_X
     MLA_Centerx(k) = -L_X/2+i*Pitch+ML_Center;
     k=k+1;
 end
@@ -81,41 +94,28 @@ MLA_Centery = MLA_Centerx;
 [MLA_CenterX,MLA_CenterY]=meshgrid(MLA_Centerx,MLA_Centery);
 
 % Wavefront calculation (using the "zernike_8_deriv.m" program
-[WF_derivY,WF_derivX]=zernike_8_deriv(MLA_CenterX/ApertRad,...
-    MLA_CenterY/ApertRad,z11,zi11,z02,z22,zi22,z13,zi13,z04);
+[WF_derivY,WF_derivX]=zernike_8_deriv(MLA_Centerx/ApertRad,...
+    MLA_Centery/ApertRad,z11,zi11,z02,z22,zi22,z13,zi13,z04);
 
 % Shift calculation
-ShiftX=WF_derivX * ML_focal/ApertRad;
-ShiftY=WF_derivY * ML_focal/ApertRad;
-
-% For setting shifts individually:
-% ShiftX = ones(20)*PixelSize;      % the offset is equal to the pixel size 
-% ShiftY = zeros(20);               % the offset is equal to zero
+ShiftX=WF_derivX * ML_focal/ApertRad/PixelSize;
+ShiftY=WF_derivY * ML_focal/ApertRad/PixelSize;
 
 %%  Hartmannogram of the distorted wavefront
-% tic
-% IntXY=zeros(ML_Pixels); 
-H={};
-for N=1:NumberLens_X
-    for M=1:NumberLens_Y
-        Intensity1 =(sinc(Pitch*(X1 + ShiftX(M,N))/Lambda/ML_focal)).^2.*...
-            (sinc(Pitch*(Y1 + ShiftY(M,N))/Lambda/ML_focal)).^2;
-    % Сonversion to digitized intensity in the camera (by bit)
-    MaxIntens=max(max(Intensity1(:,:),[],1));
-    MinIntens=MaxIntens/(2^Bits-1);
-    IntensityPixelised=fix(Intensity1(:,:)./MinIntens).*MinIntens/max(max(Intensity1)); 
-    % Recording an intensity pattern for one subaperture (N,M)
-    H{N,M}=IntensityPixelised;
-    end
-end
-% toc
-% Сombining into a single matrix (hartmannogram)
-H=cell2mat(H);
-% Output of the reference hartmannogram
+ShiftX_rep = repelem(ShiftX,ML_Pixels);
+ShiftY_rep = repelem(ShiftY,ML_Pixels);
+[SHIFT_X,SHIFT_Y] = meshgrid(ShiftX_rep,ShiftY_rep);
+
+VAL_shift = (sinc(Pitch*PixelSize*(X+SHIFT_Y)/Lambda/ML_focal)).^2.*...
+    (sinc(Pitch*PixelSize*(Y+SHIFT_X)/Lambda/ML_focal)).^2;
+MaxIntens=max(max(VAL_shift(:,:),[],1));
+MinIntens=MaxIntens/(2^Bits-1);
+IntensityPixelised_VAL_shift=fix(VAL_shift(:,:)./MinIntens).*MinIntens/max(max(VAL_shift));
+
 figure('Name','Processed hartmannogram');
-imshow(H,[]);
+imshow(IntensityPixelised_VAL_shift,[]);
 % Save to the "Hartmannograms" folder 
-imwrite(H,'Hartmannograms\Hartmannogram_DEF_500_nointegr_136.bmp');
+imwrite(IntensityPixelised_VAL_shift,'Hartmannograms\DEF_2048_136_val_shift.bmp'); 
 
 %%  Writing reference and shifts to a file 
 % % Data for reference
@@ -151,7 +151,7 @@ imwrite(H,'Hartmannograms\Hartmannogram_DEF_500_nointegr_136.bmp');
 % Shift_Centers(2:2:end,:) = New_CenterY;
 % 
 % % Creating a reference file
-% fig1 = fopen('Files_txt\Reference_nointeg.txt', 'w'); 
+% fig1 = fopen('Files_txt\Reference.txt', 'w'); 
 % fprintf(fig1, '%d\n',Pixels_X);             % image width in pixels
 % fprintf(fig1, '%d\n',Pixels_Y);             % image height in pixels
 % fprintf(fig1, '%5.6f\n',Lambda*10^6);       % wavelength [um]
@@ -162,6 +162,6 @@ imwrite(H,'Hartmannograms\Hartmannogram_DEF_500_nointegr_136.bmp');
 % fclose(fig1);
 % 
 % % Creating a processed file
-% fig2 = fopen('Files_txt\Shifted_centers_nointeg.txt', 'w'); 
+% fig2 = fopen('Files_txt\Shifted_centers.txt', 'w'); 
 % fprintf(fig2, '%5.10f\n', Shift_Centers);   % shifting the centers [m]
 % fclose(fig2);
